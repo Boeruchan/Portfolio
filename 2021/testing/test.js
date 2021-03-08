cfd6f064787be47de65a649982ee9e2f31f26e9e
Boeruchan <chingman.fung@gmail.com>
2021-03-08 09:47:57 +0100

    portfolio
    woeespiedaassiieee

--- 2021/testing/test.js
+++ 2021/testing/test.js
@@ -1,16 +1,52 @@
 // import Swup from 'swup';
 const swup = new Swup();
 
-document.querySelector('body').addEventListener('mousemove', eyeball);
+var canvas = document.getElementById("canvas");
+var ctx = canvas.getContext("2d");
+var radius = canvas.height / 2;
+ctx.translate(radius, radius);
+radius = radius * 0.90
+drawClock();
 
-function eyeball(){
-    let eye = document.querySelectorAll(".eye");
-    eye.forEach(function(eye){
-        let x  = (eye.getBoundingClientRect().left) + (eye.clientWidth / 2);
-        let y  = (eye.getBoundingClientRect().top) + (eye.clientHeight / 2);
-        let radian = Math.atan2(event.pageX - x, event.pageY - y);
-        let rot = (radian * (180 / Math.PI) * -1) + 0;
-        eye.style.transform = "rotate(" + rot + "deg)";
-    })
-
+function drawClock() {
+  drawFace(ctx, radius);
+  drawNumbers(ctx, radius);
 }
+
+function drawFace(ctx, radius) {
+  var grad;
+  ctx.beginPath();
+  ctx.arc(0, 0, radius, 0, 2*Math.PI);
+  ctx.fillStyle = 'white';
+  ctx.fill();
+  grad = ctx.createRadialGradient(0,0,radius*0.95, 0,0,radius*1.05);
+  grad.addColorStop(0, '#333');
+  grad.addColorStop(0.5, 'white');
+  grad.addColorStop(1, '#333');
+  ctx.strokeStyle = grad;
+  ctx.lineWidth = radius*0.1;
+  ctx.stroke();
+  ctx.beginPath();
+  ctx.arc(0, 0, radius*0.1, 0, 2*Math.PI);
+  ctx.fillStyle = '#333';
+  ctx.fill();
+}
+
+function drawNumbers(ctx, radius) {
+  var ang;
+  var num;
+  ctx.font = radius*0.15 + "px arial";
+  ctx.textBaseline="middle";
+  ctx.textAlign="center";
+  for(num = 1; num < 13; num++){
+    ang = num * Math.PI / 6;
+    ctx.rotate(ang);
+    ctx.translate(0, -radius*0.85);
+    ctx.rotate(-ang);
+    ctx.fillText(num.toString(), 0, 0);
+    ctx.rotate(ang);
+    ctx.translate(0, radius*0.85);
+    ctx.rotate(-ang);
+  }
+}
+
