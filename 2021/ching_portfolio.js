e9d12d1e1632fb7701887e44a14eedeb68d9e60c
Boeruchan <chingman.fung@gmail.com>
2021-03-10 09:44:40 +0100

    slide deel 2

--- 2021/ching_portfolio.js
+++ 2021/ching_portfolio.js
@@ -24,3 +24,31 @@ function setRotation(element, rotationRatio) {
 
 setClock()
 
+//slide
+
+const slides = document.querySelectorAll('.slide');
+const btns = document.querySelectorAll('.btn');
+let currentSlide = 1;
+
+const manualNav = function(manual) {
+    slides.forEach((slide) => {
+        slide.classList.remove('active');
+      
+    btns.forEach((btn) => {
+        btn.classList.remove('active');
+    });
+    });
+
+
+    slides[manual].classList.add('active');
+    btns[manual].classList.add('active');
+}
+
+btns.forEach((btn,i) => {
+    btn.addEventListener("click", () => {
+        manualNav(i);
+        currenSlide = i;
+    });
+});
+
+
